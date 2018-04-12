3cf734e8295946955a5b605b838b1b9bcfb53eaf
Noah Presby <npresby41@gmail.com>
2018-04-12 13:43:42 -0400

    begin text adventure game

---
+++ text_adventure.py
@@ -0,0 +1,50 @@
+print("WELCOME!")
+print("TO THE MAGNIFICENT ADVENTURES OF LIL YACHTY")
+print()
+print()
+start = input("Press 'y' to start; 'n' to terminate everything ever ")
+
+if start == 'n':
+    while True:
+        print("0110100001100110011011110111010100111011011100110110010001100110011101010110100101101000011001100110100001101001011101010110011001101000011101010110111101110011011001000110011001101000011011110110100101110101011001010110100001110101011010010110011001100111011001010111010101101001011001100110100001110101011010010110010001110011011001100111010101101011011000010110010001101000011001100111010101100101")
+
+if start == 'y':
+    print("Your name is Lil Yachty. You have one mission. Terminate all thots.")
+    story_1 = input("Do you accept your mission(y/n)? ")
+
+    if story_1 == 'n':
+        print("Agent Yachty, you have failed us...Prepare to be terminated.")
+        while True:
+            print("0110100001100110011011110111010100111011011100110110010001100110011101010110100101101000011001100110100001101001011101010110011001101000011101010110111101110011011001000110011001101000011011110110100101110101011001010110100001110101011010010110011001100111011001010111010101101001011001100110100001110101011010010110010001110011011001100111010101101011011000010110010001101000011001100111010101100101")
+
+    if story_1 == 'y':
+        print()
+        print("OK, your first target is Lil Pump. His thottery must cease!")
+        print("*As you approach Lil Pump, he becomes wary and pulls out his saw*")
+        print()
+        first_choice = input("Do you pull out your Pressure Washer to spray him(press 1)\nOr do you want to wait a little longer and see what he does(press 2) ")
+
+        if first_choice == '1':
+            print("Lil Pump is immediately evaporated by the high pressure")
+            print("~Mission 1 Complete~")
+            print()
+            print("Your next mission involves interrogating Trippie Redd for information about the Dark Knight, Dummo")
+            print()
+            print("*You get to Trippie's House and you try to figure out the best way to handle it*")
+            second_choice = input("Do you establish a perimeter and check for snipers(1)\nOr do you want to charge in the house beads-first(2) ")
+
+            if second_choice == '1':
+                print("As you look into a window, checking for snipers, Trippie's Henchman, Romeo and Juliet, attack you\nwhere you will never be heard from again")
+                print("GAME OVER!")
+
+            if second_choice == '2':
+                print("Oddly enough, chraging in works out.\nAs you charge in, you slam into Trippie Redd himself!")
+                print("You trample him and receive information about the whereabouts of the Dark Knight, Dummo")
+                print()
+                print("Your next task is to meet with Special Agent Uzi Vert")
+                print("He will debrief you on the next part of your mission")
+                print()
+                print("Greetings Agent Yachty
+                
+
+
